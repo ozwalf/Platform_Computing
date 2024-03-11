dcebb74012a671b7f49c8f6c502d63a80ce167e5
ozwalf <yuyugunfire6@gmail.com>
2024-03-11 10:24:32 -0700

    csv format change
    t

--- Assignment_2_User_Interaction/metric_tracker/metric_tracker.py
+++ Assignment_2_User_Interaction/metric_tracker/metric_tracker.py
@@ -13,11 +13,11 @@ def main():
     driver.get("http://localhost:3000/")
 
     metrics = [
-        ['Current Time', 'Metric', 'Value']
+        ['Current Time', 'Page Title', 'Presence Time', 'Scroll Length', 'Button Clicks']
     ]
 
     title = driver.title
-    metrics.append(['---', 'Page Title', title])
+    # metrics.append(['---', 'Page Title', title])
 
     # button clicks
     num_clicks = 0
@@ -41,14 +41,14 @@ def main():
         presence_time = current_time - start_time
         print(f"Presence time: {presence_time} seconds")
         # appends presence time per iteration
-        metrics.append([current_time_string, 'Presence time', presence_time])
+        # metrics.append([current_time_string, 'Presence time', presence_time])
         
         # Track scrolling
         scroll_height = driver.execute_script("return document.body.scrollHeight")  
         current_scroll = driver.execute_script("return window.pageYOffset")
         print(f"Scrolled {current_scroll}/{scroll_height} pixels")
         # appends scroll length per iteration
-        metrics.append([current_time_string, 'Scroll length', f"{current_scroll}/{scroll_height}"])
+        # metrics.append([current_time_string, 'Scroll length', f"{current_scroll}/{scroll_height}"])
         
         time.sleep(2) 
         
@@ -62,13 +62,11 @@ def main():
             
         print(f"Number of clicks: {num_clicks}")
         # appends total number of click after each iteration of the while loop
-        metrics.append([current_time_string, 'Button clicks', num_clicks])
+        # metrics.append([current_time_string, 'Button clicks', num_clicks])
 
-    # appends final presence time after the loop
-    # metrics.append(['Presence time', presence_time])
+        metrics.append([current_time_string, title, presence_time, f"{current_scroll}/{scroll_height}", num_clicks])
 
-    # appends total number of click after the loop
-    # metrics.append(['Button clicks', num_clicks])
+
         
     # writes data into csv file
     with open('metric_tracker.csv', 'w', newline='') as file:
